551300a3e7cbd3675fb5a435100f0019b705f50b
NadanasserShalaby <shalabynadanasser@gmail.com>
2025-02-06 22:38:37 +0200

    first commit

--- src/Component/Layout/Layout.jsx
+++ src/Component/Layout/Layout.jsx
@@ -10,15 +10,11 @@ export default function Layout() {
 
     <>
     <Sidebar_1 onSelect={() => {}}  />
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-6">
+      <div className="flex flex-col items-center justify-center m-auto text-center p-20">
             <h1 className="text-9xl font-extrabold text-gray-800">404</h1>
             <p className="text-xl text-gray-600 mt-2">Oops! The page you're looking for doesn't exist.</p>
             
-            <img 
-                src="https://cdni.iconscout.com/illustration/premium/thumb/astronaut-lost-in-space-3676331-3116761.png"
-                alt="Lost Astronaut"
-                className="w-72 my-6"
-            />
+            
 
             <Link to="/" className="px-6 py-3 bg-black text-white rounded-lg shadow-md hover:bg-gray-800 transition">
                 Go Back Home
